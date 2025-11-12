9887b13881cbcc3d12ae580d48e2bb9dcb95a918
frostybee <rsleiman2@gmail.com>
2025-11-12 11:55:57 -0500

    Update composer.bat

--- composer.bat
+++ composer.bat
@@ -1,5 +1,13 @@
 @ECHO OFF
 REM This is a batch file that calls the composer.bat file in the Wampoon's root directory.
 REM Use it to run composer for installing and updating dependencies for the project.
-call "../../composer.bat" %*
-exit /b %errorlevel%
+
+set "COMPOSER_PATH=..\..\composer.bat"
+
+IF NOT EXIST "%COMPOSER_PATH%" (
+    ECHO Required composer.bat not found at "%COMPOSER_PATH%".
+    EXIT /B 1
+)
+
+CALL "%COMPOSER_PATH%" %*
+EXIT /B %ERRORLEVEL%
